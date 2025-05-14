6b5c93b73ba6316f749e0fb5a5f570c11084a30e
Saidov Zohid <116626702+SaidovZohid@users.noreply.github.com>
2025-05-14 11:19:34 +0500

    fix: added the paste without overwriting register

--- init.lua
+++ init.lua
@@ -171,6 +171,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Set <leader>p to "\"_dP"
+vim.keymap.set('x', '<leader>p', '"_dP', { desc = 'Paste without overwriting register' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
