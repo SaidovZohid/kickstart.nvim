fc2bdc4f3b94555e35e65092b6c76a5317b7042d
Zohid Saidov <zohidsaidov17@gmail.com>
2026-05-29 03:48:44 +0500

    feat: add proto syntax overrides for kanagawa

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,20 @@ return {
     ---@diagnostic disable-next-line: missing-fields
     require('kanagawa').setup {
       colors = { theme = { all = { ui = { bg_gutter = 'none' } } } },
+      overrides = function(colors)
+        local theme = colors.theme
+
+        return {
+          ['@keyword.proto'] = { fg = theme.syn.keyword, italic = true },
+          ['@keyword.import.proto'] = { fg = theme.syn.preproc },
+          ['@keyword.function.proto'] = { fg = theme.syn.fun, bold = true },
+          ['@keyword.return.proto'] = { fg = theme.syn.special3, italic = true },
+          ['@type.proto'] = { fg = theme.syn.type },
+          ['@property.proto'] = { fg = theme.syn.identifier },
+          ['@variable.proto'] = { fg = theme.syn.identifier },
+          ['@variable.member.proto'] = { fg = theme.syn.identifier },
+        }
+      end,
       theme = 'dragon',
       globalStatus = true,
       background = {
